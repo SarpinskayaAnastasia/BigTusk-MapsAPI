7283e7eed93ce8d6afea111ae8eca023ef6df89e
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2021-02-12 18:17:18 +0300

    first tusk

--- programm_file_too.py
+++ programm_file_too.py
@@ -1,32 +1,63 @@
 import os
-import sys
-
 import pygame
 import requests
+import sys
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"response = requests.get(map_request)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+# Создайте оконное приложение, отображающее карту по координатам и в масштабе, который задаётся программно.
 
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+class MapParams(object):
+    def __init__(self):
+        self.lat = 61.665279  # Координаты центра карты на старте. Задал координаты университета
+        self.lon = 50.813492
+        self.zoom = 16  # Масштаб карты на старте. Изменяется от 1 до 19
+        self.type = "map"  # Другие значения "sat", "sat,skl"
 
-# Инициализируем pygame
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    # Преобразование координат в параметр ll, требуется без пробелов, через запятую и без скобок
+    def ll(self):
+        return str(self.lon) + "," + str(self.lat)
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запись полученного изображения в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+    return map_file
+
+
+def main():
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    mp = MapParams()
+    map_file = load_map(mp)
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+        # Создаем файл
+        map_file = load_map(mp)
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    # Удаляем файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
